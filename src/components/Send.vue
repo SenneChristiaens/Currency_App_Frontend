7f3f37c27bdb77f38fc54ed9256c4faf58ee085b
Senne <>
2022-05-17 22:17:30 +0200

    feat: added send function

--- src/components/Send.vue
+++ src/components/Send.vue
@@ -1,5 +1,32 @@
 <script setup>
+import { ref } from "vue";
+
 const emit = defineEmits(["view"]);
+let receiver = ref("");
+let amount = ref("");
+
+function Send() {
+  let data = {
+    receiver: receiver,
+    amount: amount,
+  };
+  fetch("http://localhost:3001/api/v1/transactions/create", {
+    method: "POST", // or 'PUT'
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data != undefined) {
+        emit("view", "transactions");
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
 </script>
 
 <template>
@@ -8,7 +35,12 @@ const emit = defineEmits(["view"]);
   </button>
   <div>
     <h1>Send coins</h1>
-    <input type="text" placeholder="Receiver's email" v-model="receiver" class="input" />
+    <input
+      type="text"
+      placeholder="Receiver's email"
+      v-model="receiver"
+      class="input"
+    />
     <input type="number" placeholder="Amount" v-model="amount" class="input" />
     <button @click="Send" class="btn">Send</button>
   </div>
